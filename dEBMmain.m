8fa25925bf8cb5f8b4fb46f0de767c4340ad8332
ukrebska <30793691+ukrebska@users.noreply.github.com>
2018-11-22 16:32:09 +0100

    Add files via upload

---
+++ dEBMmain.m
@@ -0,0 +1,51 @@
+function melt=dEBMmain(lat,swd,T,A)
+% please cite:
+% Krebs-Kanzow, U., Gierz, P., and Lohmann, G.:
+% Brief communication: An Ice surface melt scheme including the diurnal cycle
+% of solar radiation,
+% The Cryosphere Discuss.,
+% https://doi.org/10.5194/tc-2018-130, in press
+% (C) Uta Krebs-Kanzow, Alfred Wegener Institute, Bremerhaven, Germany, 2018
+%******************************************************************************
+% A,lat,swd,PDD are here expexted to be 
+% fields of size (xdim x ydim x 12 x number of years) 
+% 
+
+% parameters defined for dEBM 
+[xdim,ydim,bla,NY] = size(swd);
+dEBMmelt           = zeros(xdim,ydim,bla,NY); (initialize)
+epsa   = .76;     % atm. emissivity
+epsi   = .95;     % emissivity of ice
+beta   = 10;      % turbulent heat transfer coeff
+bolz   = 5.67e-8; % Stefan-Boltzmann constant
+T0     = 273.15;  % melt point in K
+Tmin   = -6.5;    % background melt condition
+c1     = (epsa*4*bolz*T0^3+beta);   
+c2     = (-epsi+epsa*epsi)*bolz*T0^4;
+elev   = 17.5
+obl    =23.44;
+mth    = 1:12     
+
+% generate parameters that depend on latitude and month 
+[hours,q,bla] = dEBM_sunny_hours_c(mth,lat,elev,obl);
+
+% inflate parameters to create (xdim x ydim x 12 x number of years) 
+hours = hours(:,:,:,ones(NY,1));       
+q     = q(:,:,:,ones(NY,1));
+
+% calculte PDD 
+PDD   = PDD4(T,3.5);
+
+% melt condition: ice is warm enough to warm to melting point during daytime	
+MC    = (T(:,:,mth,i)>Tmin);            
+
+% calculate monthly melt rates		
+for i=1:NY
+   for mth=1:12	
+       dEBMmelt(:,:,mth,i) = dEBMmodel(A(:,:,mth,i),...
+				       swd(:,:,mth,i),...
+				       PDD(:,:,mth,i),...
+				       hours(:,:,mth,i),...
+				       q(:,:,mth,i),c1,c2,MC);
+   end
+end
