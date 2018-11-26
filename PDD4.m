e993f6db70a1107dd5d36a0daa5b8ea96ce7977e
ukrebska <30793691+ukrebska@users.noreply.github.com>
2018-11-26 11:59:54 +0100

    Add files via upload

---
+++ PDD4.m
@@ -0,0 +1,13 @@
+function PDD = PDD(T,stddev)
+% PDD is approximated as described in
+% Calov R and Greve R (2005)
+% Correspondence. A semi-analytical solution for the positive degree-day model
+% with stochastic temperature variations.
+% J. Glaciol., 51 (172), 173–175
+% (doi:10.3189/172756505781829601)
+%****************************************************************************
+% T in ^oC!!!
+% stddev can be spatially variable (same grid as T) or a constant.
+%T=T-273.15;
+
+PDD=stddev./sqrt(2*pi).*exp(-T.^2./(2*stddev.^2))+T/2.*erfc(-T./(sqrt(2)*stddev));
