7077d9e37ed15c9116f4411594df94bf481ab602
徐杉 <xushan@awi.de>
2018-12-11 13:44:46 +0100

    remove .m from main folder

--- dEBM_sol_flux_fact.m
+++
@@ -1,6 +0,0 @@
-function solfluxfact=dEBM_sol_flux_fact(sin_phi_sin_d,cos_phi_cos_d,ha)
-% if F is the flux density normal to a surface,
-% insolation between [-ha,ha] is SW=  sol_flux_fact*F
-% ha is an hour angle during day time
-	    
-solfluxfact=(sin_phi_sin_d.*ha+cos_phi_cos_d.*sin(ha))/pi;
