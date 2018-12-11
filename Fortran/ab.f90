44d87c66ca78b7517261ffc6c476dc01cf7fcf7f
徐杉 <xushan@awi.de>
2018-12-11 14:09:20 +0100

    ad1 works, ab not yet

--- Fortran/ab.f90
+++ Fortran/ab.f90
@@ -2,15 +2,16 @@ PROGRAM TEST
   implicit none
   include 'netcdf.inc'
   integer :: ierr,ncid,varid,varidx,varidy,len_file,err
-  ! real, dimension(:,:,:,:), allocatable :: swd,A,T
-  ! real, dimension(:,:), allocatable :: x,y
-  ! integer :: xdim,ydim,tdim,NY
+  real, dimension(:,:,:,:), allocatable :: swd,A,T
+  real, dimension(:,:), allocatable :: x,y
+  integer, allocatable, dimension(:,:,:) :: dims
+  integer :: xdim,ydim,tdim,NY
 
-  integer,parameter :: xdim=192,ydim=96,tdim=12,NY=1
-  real  :: x(xdim,1)
-  real  :: y(ydim,1)
-  ! real  :: lon(LLm,MMm),lat(LLm,MMm)
-  real  :: swd(xdim,ydim,tdim,NY),A(xdim,ydim,tdim,NY),T(xdim,ydim,tdim,NY)
+  ! integer,parameter :: xdim=192,ydim=96,tdim=12,NY=1
+  ! real  :: x(xdim,1)
+  ! real  :: y(ydim,1)
+  ! ! real  :: lon(LLm,MMm),lat(LLm,MMm)
+  ! real  :: swd(xdim,ydim,tdim,NY),A(xdim,ydim,tdim,NY),T(xdim,ydim,tdim,NY)
   !
   character*300 :: file_in
 
@@ -20,18 +21,24 @@ PROGRAM TEST
   ierr=nf_inq_varid(ncid,'rsuscs_na',varid)
   ierr=nf_get_var_real(ncid,varid,swd)
 
-  ! xdim   = size(swd,dim=1)
-  ! ydim   = size(swd,dim=2)
-  ! tdim   = size(swd,dim=3)
-  ! NY     = size(swd,dim=4)
+  ! dims = shape(swd)
+  write(*,*) shape(swd)
 
-  ! write(*,*) xdim,ydim,tdim,NY
+ write(*,*) swd(159,45,1,1),swd(154,4,1,1)
 
-  ! allocate (x(xdim,ydim))
-  ! allocate (y(xdim,ydim))
-  ! allocate (T(xdim,ydim,tdim,NY))
-  ! allocate (A(xdim,ydim,tdim,NY))
-  ! allocate (swd(xdim,ydim,tdim,NY))
+
+  ! xdim   = size(swd[:], 1)
+  ! ydim   = size(swd[:], 2)
+  ! tdim   = size(swd[:], 3)
+  ! NY     = size(swd[:], 4)
+
+  write(*,*) xdim,ydim,tdim,NY
+
+  !allocate (x(xdim,ydim))
+  !allocate (y(xdim,ydim))
+  !allocate (T(xdim,ydim,tdim,NY))
+  !allocate (A(xdim,ydim,tdim,NY))
+  !allocate (swd(xdim,ydim,tdim,NY))
   !
   ! ierr=nf_inq_varid(ncid,'temp2',varid)
   ! ierr=nf_get_var_real(ncid,varid,T)
@@ -42,6 +49,6 @@ PROGRAM TEST
   ! ierr=nf_get_var_real(ncid,varidx,x)
   ! ierr=nf_get_var_real(ncid,varidy,y)
   !
-  print *,swd(159,45,1,1),swd(154,4,1,1)
+  !print *,swd(159,45,1,1),swd(154,4,1,1)
   err=NF_CLOSE(ncid)
 END PROGRAM TEST
