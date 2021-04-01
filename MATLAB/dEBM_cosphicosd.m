3175e6fddbca5b7cadc420f6c92d39c2857e7263
sxu <sxu@awi.de>
2021-04-01 18:18:16 +0200

    upload test file

--- MATLAB/dEBM_cosphicosd.m
+++
@@ -1,4 +0,0 @@
-function cosphicosd=dEBM_cosphicosd(lat,decl)
-% again just for optimization  
-cosphicosd=cos(pi/180*lat).*cos(pi/180*decl);
-
--- MATLAB/dEBM_day.m
+++
@@ -1,4 +0,0 @@
-function days = dEBM_day()
-% for the middle of each month we calculate the number of days since
-%  new year in a Julian calendar (very simple)
-days=round(.5*(cumsum([31;28;31;30;31;30;31;31;30;31;30;31])+cumsum([0;31;28;31;30;31;30;31;31;30;31;30])));
